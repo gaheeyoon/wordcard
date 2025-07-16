28063de060437fff1403306c484d333b3bf40f20
gaheeyoon <a5312559@gmail.com>
2025-07-17 07:33:31 +0900

    일자별로 단어장을 볼 수 있게 수정

---
+++ books.js
@@ -0,0 +1,10 @@
+const books = {
+    "0001": {
+        storyUrl: "https://www.youtube.com/watch?v=eb-ckBZxrjY",
+        storybookUrl: "https://www.littlefox.com/en/supplement/ebook/ajBnMmFpb1NBTytlQ0VlOEdPWVhsQT09"
+    },
+    "0002": {
+        storyUrl: "https://www.youtube.com/watch?v=ab17KuvePcA",
+        storybookUrl: "https://www.littlefox.com/en/supplement/ebook/WDdXOTJHOUxKc21EZUtsdFJiUmtjUT09"
+    }
+};
