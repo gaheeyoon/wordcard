07f371e9add145b8513ebe60dd9ae18831924af0
gaheeyoon <a5312559@gmail.com>
2025-07-24 20:06:35 +0900

    3일차 언어 업데이트

--- books.js
+++ books.js
@@ -1,10 +1,19 @@
 const books = {
-    "0001": {
-        storyUrl: "https://www.youtube.com/watch?v=eb-ckBZxrjY",
-        storybookUrl: "https://www.littlefox.com/en/supplement/ebook/ajBnMmFpb1NBTytlQ0VlOEdPWVhsQT09"
+    "FS0127-01": {
+        storyUrl: "https://www.littlefox.co.kr/ko/readers/contents_list_v2/FS0127",
+        storybookUrl: "https://www.littlefox.co.kr/ko/supplement/ebook/ajBnMmFpb1NBTytlQ0VlOEdPWVhsQT09"
     },
-    "0002": {
-        storyUrl: "https://www.youtube.com/watch?v=ab17KuvePcA",
-        storybookUrl: "https://www.littlefox.com/en/supplement/ebook/WDdXOTJHOUxKc21EZUtsdFJiUmtjUT09"
+    "FS0127-02": {
+        storyUrl: "https://www.littlefox.co.kr/ko/readers/contents_list_v2/FS0127",
+        storybookUrl: "https://www.littlefox.co.kr/ko/supplement/ebook/WDdXOTJHOUxKc21EZUtsdFJiUmtjUT09"
+    },
+    "FS0127-03": {
+        storyUrl: "https://www.littlefox.co.kr/ko/readers/contents_list_v2/FS0127",
+        storybookUrl: "https://www.littlefox.co.kr/ko/supplement/ebook/cnhuRkRkVmM0amRiUThoNzBRZ2YvUT09"
+    },
+
+
+    "wonbuk-01": {
+        
     }
 };
